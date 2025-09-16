54b22328ee4e98ccba25dd9ff91252ada493d225
Arthur Lemarchand <arthlemarchand@gmail.com>
2025-09-16 19:01:05 +0200

    Stop suivre les .js (ignore via .gitignore); garder uniquement les .ts

--- Main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Car_1 = require("./Car");
-const race_1 = require("./race");
-const a = new Car_1.Car("Model Y", "Tesla", "jaune fluo", 2021);
-const b = new Car_1.Car("500", "Fiat", "rose", 2023);
-const c = new Car_1.Car("Twingo", "Renault", "noire", 2020);
-(0, race_1.race)([a, b, c], { laps: 5, lapKm: 3 });
